996f34f22227143a262cb33332ef0c7f84fc2611
guoronline <guor@kust.edu.cn>
2022-06-16 21:17:53 +0800

    Create index.js

---
+++ www/ZY/src/lib/dexie/index.js
@@ -0,0 +1,25 @@
+import history from './history'
+import mini from './mini'
+import setting from './setting'
+import shortcut from './shortcut'
+import star from './star'
+import sites from './sites'
+import search from './search'
+import iptv from './iptv'
+import channelList from './channelList'
+import recommendation from './recommendation'
+import cachedMovies from './cachedMovies'
+
+export {
+  history,
+  mini,
+  setting,
+  shortcut,
+  star,
+  sites,
+  iptv,
+  channelList,
+  search,
+  recommendation,
+  cachedMovies
+}
